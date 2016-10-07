e1a8002f120d16bd67d792e6236af6f365b11026
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-10-07 14:18:07 +0200

    modify main

--- src/main/java/com/epam/tm/oop/Main.java
+++ src/main/java/com/epam/tm/oop/Main.java
@@ -1,17 +1,22 @@
 package com.epam.tm.oop;
 
+import com.epam.tm.oop.actions.CountAllCost;
 import com.epam.tm.oop.actions.SortByConsuption;
 
 public class Main {
     public static void main(String[] args) {
 
         TaxiStation taxi = TaxiStation.Builder.CreateTestTaxiStation();
-
-
+        System.out.println(taxi);
 
         SortByConsuption s = new SortByConsuption();
         taxi.sort(s);
 
+        System.out.println();
         System.out.println(taxi);
+
+        CountAllCost count = new CountAllCost();
+        System.out.println("Сумма всех ТС: " + taxi.getAllCost(count));
+
     }
 }
