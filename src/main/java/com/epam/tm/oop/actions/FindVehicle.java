0f55837c2e690796fded3082463736be4af289c2
Tychkov Maxim <tichkov_maxim@mail.ru>
2016-10-07 14:28:52 +0200

    add findVehicle test implementation

--- src/main/java/com/epam/tm/oop/actions/FindVehicle.java
+++ src/main/java/com/epam/tm/oop/actions/FindVehicle.java
@@ -2,8 +2,10 @@ package com.epam.tm.oop.actions;
 
 import com.epam.tm.oop.entities.Vehicle;
 
+import java.util.List;
+
 public interface FindVehicle {
 
-    Vehicle find();
+    Vehicle find(List<Vehicle> vehicles,int parameter);
 
 }
--- src/main/java/com/epam/tm/oop/actions/FindVehicleBySpeed.java
+++ src/main/java/com/epam/tm/oop/actions/FindVehicleBySpeed.java
@@ -2,9 +2,17 @@ package com.epam.tm.oop.actions;
 
 import com.epam.tm.oop.entities.Vehicle;
 
+import java.util.List;
+
 public class FindVehicleBySpeed implements FindVehicle {
     @Override
-    public Vehicle find() {
+    public Vehicle find(List<Vehicle> vehicles, int maxSpeed) {
+        for(Vehicle r : vehicles)
+            if (r.getMaxSpeed() >= maxSpeed) {
+                return r;
+            }
         return null;
     }
+
+
 }
